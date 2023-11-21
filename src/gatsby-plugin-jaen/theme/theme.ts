b01f3f1ffaaa5dfcb98e8f31f136092fb7b8c874
Florian Kleber <kleber@snek.at>
2023-11-21 15:47:22 +0100

    feat: update theme.ts

--- src/gatsby-plugin-jaen/theme/theme.ts
+++ src/gatsby-plugin-jaen/theme/theme.ts
@@ -19,7 +19,19 @@ const theme = extendTheme(
     },
     colors: {
       ...baseTheme.colors,
-      brand: {...baseTheme.colors.blackAlpha, 500: '#000000'}
+      brand: {
+        50: '#fef2f3',
+        100: '#fce7e8',
+        200: '#f9d2d7',
+        300: '#f4adb5',
+        400: '#ed7f8e',
+        500: '#e25169',
+        600: '#cd3152',
+        700: '#bb2649',
+        800: '#91203e',
+        900: '#7c1f3a',
+        950: '#450c1b',
+      },
     }
   },
   withDefaultColorScheme({
